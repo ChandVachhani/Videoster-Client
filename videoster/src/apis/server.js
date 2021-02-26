15c24637fece4954bc64f67711e70628270b422a
“Chand” <18bce249@nirmauni.ac.in>
2021-02-26 17:59:29 +0530

    small changes

--- videoster/src/apis/server.js
+++ videoster/src/apis/server.js
@@ -3,13 +3,3 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:3001",
 });
-
-// axios.defaults.headers.common['Authorization'] = localStorage.getItem("token");
-
-// axios.interceptors.request.use(function (config) {
-//   const token = localStorage.getItem("token");
-//   console.log(token);
-//   config.headers.Authorization = token;
-
-//   return config;
-// });

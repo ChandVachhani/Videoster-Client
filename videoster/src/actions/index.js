a0a51ff8a45a030159cc8bbc625e67634987f2dc
ChandVachhani <chandvachhani4441@gmail.com>
2021-07-12 19:08:36 +0530

    small changes

--- videoster/src/actions/index.js
+++ videoster/src/actions/index.js
@@ -5,19 +5,20 @@ import { createNotification } from "../utils/createNotification";
 // TODO make clear logic about verify login
 export const verifyLogin = () => {
   return async (dispatch) => {
-    try {
+    try {      
       const res = await server.post("/auth/verifyLogin", {}, {
         headers: {
           Authorization: `Basic ${localStorage.getItem("VideosterToken")}`,
         },
-      });
-      createNotification("error", res.data.message)();
+      });      
       console.log(res.data.user);
+      await localStorage.setItem("VideosterUserId", res.data.user.userId);
+      await localStorage.setItem("VideosterUserName", res.data.user.userName);
       dispatch({    
         type: "VERIFY_LOGIN",
         payload: {
-          userId: localStorage.getItem("VideosterUserId"),
-          userName: localStorage.getItem("VideosterUserName"),
+          userId: res.data.user.userId,
+          userName: res.data.user.userName,
         },
       });      
     } catch (err) {
